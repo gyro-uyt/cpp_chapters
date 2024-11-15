0cd4188dbb0ece366dc4d78c387c247322ac6884
BXaura <aura.uyt@gmail.com>
2024-11-15 12:24:21 +0530

    kraken

--- Chapter 0/0.bx.in.cpp
+++ Chapter 0/0.bx.in.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter the no. of times you want '*' to print: ";
+    cout<<"Enter the number of times you want '*' to print: ";
     cin>>n;
     for (int i = 0; i < n; i++)
     {
