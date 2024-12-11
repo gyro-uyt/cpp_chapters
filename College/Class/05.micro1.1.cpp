45fa8a5ed2e03eb2e79670a764f0be587b56edc8
BXaura <aura.uyt@gmail.com>
2024-12-11 19:38:01 +0530

    micro

--- College/Class/05.micro1.1.cpp
+++ College/Class/05.micro1.1.cpp
@@ -1,36 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int pass1, pass2, user, input;
-    cout<<"\n\t>>> Welcome to Password Management System <<<"<<endl;
-    cout<<"Enter your Username: ";
-    cin>>user;
-    cout<<"Enter the Password: ";
-    cin>>pass1;
-    cout<<"Re-enter the Password: ";
-    cin>>pass2;
-    cout<<endl;
+    cout << "\n\t>>> Welcome to Password Management System <<<" << endl;
+    int master;
+    cout<<"\nCarefully choose a Master Password: ";
+    cin>>master;
+    
+    cout << "Enter your Username: ";
+    cin >> user;
+    cout << "Enter the Password: ";
+    cin >> pass1;
+    cout << "Re-enter the Password: ";
+    cin >> pass2;
+    cout << endl;
     if (pass1 == pass2)
     {
-        cout<<"Congratulations, you're Username and Password has been saved.";
+        cout << "Congratulations, you're Username and Password has been saved.";
     }
-    else{
-        cout<<"ERROR: Enter the same Password in both fields.";
-    }
-
-    cout<<"\n.........................................................................................................................................\n";
-    cout<<"\nTo get all Your usernames & passwords enter the Master Password\n";
-    cin>> input;
-    int master = 96;
-    if ( input == master )
+    else
     {
-        cout<< "username: " << user << " & password: " << pass1 <<endl;
-    }
-    else{
-        cout<<"WRONG MASTER PASS.  :(((\n";
+        cout << "ERROR: Enter the same Password in both fields.";
     }
 
-    cout<<endl;
+    cout << "\n.........................................................................................................................................\n";
+    cout << "\nTo get all Your usernames & passwords enter the Master Password\n";
+    cin >> input;
+    if (input == master)
+    {
+        cout << "username: " << user << " & password: " << pass1 << endl;
+    }
+    else
+    {
+        cout << "WRONG MASTER PASS.  :(((\n";
+    }
+
+    cout << endl;
     return 0;
 }
