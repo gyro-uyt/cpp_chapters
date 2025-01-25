165a94114635e83a07f5a58fe0aa6511b0bb8311
auraM <aura.uyt@gmail.com>
2025-01-25 23:07:05 +0530

    fstrings

---
+++ Chapter X01/23.fstrings.py
@@ -0,0 +1,24 @@
+intro = "Hey, I am {} and I am from {}"
+name = "Uday"
+location = "Gwalior"
+
+# OLD Method of strings filling as Variables
+# string.format(___, _____) ---> ___ will replace First{} and _____ will replace Second{}
+# order matters if no numbering is specified
+print(intro.format(name, location))
+print(intro.format(location, location))
+print(intro.format(location, name))  # we can use Numbering by specifying it
+intro1 = "Hey, I am {1} and I am from {0}"
+# location is @index 0th so it'll go to 0th no. of string
+print(intro1.format(location, name))
+
+# using fstrings
+intro2 = f"Hey, I am {name} and I am from {location}"
+print(intro2, "<--- this is from fstrings")
+print(f"{4*20}", "<--- this arithmetic in string via fstrings")
+
+txt = "For only {price:.2f} dollars!"
+print(txt.format(price = 59.54666543546))
+
+value = 45.7824235
+print(f"Cost of New CDM is {value:.3f}")    # value:.3f means it will print value upto 3 decimal points 
