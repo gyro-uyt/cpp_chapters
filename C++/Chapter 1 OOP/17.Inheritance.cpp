676213757e8a8eaba48af47fdf751db86d8a17d5
aura-mercury <aura.uyt@gmail.com>
2025-02-28 23:52:58 +0530

    Inheritance

--- C++/Chapter 1 OOP/17.Inheritance.cpp
+++ C++/Chapter 1 OOP/17.Inheritance.cpp
@@ -22,13 +22,64 @@
 #include <iostream> 
 using namespace std;
 
+// Base class
 class Employee
 {
-    
+public:
+    int id;
+    float salary;
+
+    Employee(){}
+    Employee(int empId)
+    {
+        id = empId;
+        salary = 69.0;
+    }
+    void print()
+    {
+        cout<<"Employee id:"<<id<<" has a salary of Rs."<<salary<<endl;
+    }
 };
 
+/* Derived class Syntax:
+class {{derived-class-name}} : {{visibility-mode}} {{base-class-name}}  // {{}} indicates items to be Replaced with
+{
+    class members/methods/etc.
+}*/
+/*
+Note:
+1.Default visibility mode is private.
+2.Public Visibility mode: Public members of the base class becomes Public members of the derived class.
+3.Private Visibility mode: Public members of the base class becomes Private members of the derived class.
+4.Private Members of the Base class can NEVER be inherited.
+*/
+
+// Creating a Programmer class derived from Employee base class
+class Programmer :public Employee
+{
+public:
+    Programmer(){}
+    Programmer(int pId)
+    {
+        id = pId;
+    }
+    int langLearn = 5;
+    void printData()
+    {
+        cout<<"Derived class Programmer has ID:"<<id<<endl;
+    }
+};
 int main()
 {
+    Employee n1, n2(23), n3(69);
+    n1.print();
+    n2.print();
+    n3.print();
+    
+    Programmer p1(42);
+    cout<<p1.langLearn<<endl;
+    p1.printData();
+    cout<<p1.id<<endl;  // p1.id is only accessible as we have inherited Publically
 
     return 0;
 }
---
+++ C++/Chapter 1 OOP/18.SingleInher.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+    int data1; // data1 here is private, hence cannot be Inherited
+public:
+    int data2;
+
+    void setData();
+    int getData1();
+    int getData2();
+};
+void Base ::setData()
+{
+    data1 = 10;
+    data2 = 20;
+}
+int Base ::getData1()
+{
+    return data1;
+}
+int Base ::getData2()
+{
+    return data2;
+}
+
+class Derived : public Base
+{
+    int data3;
+
+public:
+    void process();
+    void display();
+};
+void Derived ::process()
+{
+    data3 = data2 * getData1();
+}
+void Derived ::display()
+{
+    cout << "Derived class, data3 = " << data3 << endl;
+}
+
+int main()
+{
+    Base n1, n2;
+    n1.setData();
+    cout << n1.getData1() << " " << n1.getData2() << endl;
+  
+    Derived d1;
+    d1.setData();   // Note: we need initiale setData() again as it's new function of Derived class
+    d1.process();
+    d1.display();
+
+    return 0;
+}
+/*
+ How Inheritance actually Works?
+ >Inheritance does NOT copy members. It just allows access to the base class’s public and protected members.
+ >Each derived object contains a hidden base class object inside it.
+ >Base class functions are called on this internal base object.
+ >Private members exist but are inaccessible outside their class.
+ >Only constructors are not inherited—they must be explicitly called. 
+*/
+/*
+ Why are Base Class Constructors Automatically Called but Not Inherited?
+ 
+*/
