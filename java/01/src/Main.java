abcfef44a8b397901caef18a3842c810cc509b82
aura-mercury <aura.uyt@gmail.com>
2025-03-19 23:52:02 +0530

    java-jdk IntelliJ

---
+++ java/01/src/Main.java
@@ -0,0 +1,10 @@
+// jdk = java development kit, install it along with IntelliJ Idea
+// package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+    // write your code
+    System.out.println("Yokoso, aare aare");
+
+    }
+}
