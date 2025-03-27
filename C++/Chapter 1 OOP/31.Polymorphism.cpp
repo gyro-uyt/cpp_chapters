cee3f237bfc0eb30c89479b9dec782f91163767a
aura-uyt <aura.uyt@gmail.com>
2025-03-27 19:22:33 +0000

    Polymorphism

---
+++ C++/Chapter 1 OOP/31.Polymorphism.cpp
@@ -0,0 +1,24 @@
+/*
+Polymorphism: 'Poly' means many & 'phism' meaning forms, hence multiple-forms
+-> one name and multiple forms
+-> eg. function overloading, operator overloading
+-> eg. Virtual functions
+
+                    Polymorphism
+                   /            \
+            Compile Time       Run Time
+           /            \           |
+          /              \      Virtual Functions
+    Function Over.     Operator Over.
+
+Compile time ---> Compiler already knows which form to take for executing the program.
+Run time ---> Compiler doesn't knows which form to take, it depends on the user instructions.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
