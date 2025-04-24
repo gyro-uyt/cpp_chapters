cede8cd74eca05a39100c90b4cbe6c354162e183
aura-uyt <aura.uyt@gmail.com>
2025-04-24 22:50:31 +0530

    java setup on OMEN

---
+++ java/javaCWH/src/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("This is my First Code");
+    }
+}
+
+//    public static void greet(char[] args){
+//        System.out.println("This is from the greet function of Main class");
+//    }
+//public class Greet{
+//    public static void message(String[] args){
+//        System.out.println("This is from the Greet class's message function");
+//    }
+//}
