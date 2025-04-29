9595b74c987b8801bc624e1b0615f7d69ccc89a6
aura-uyt <aura.uyt@gmail.com>
2025-04-29 17:10:48 +0000

    0013 Done using Vector

--- DSA/LeetCode/0013.romanToInteger.cpp
+++ DSA/LeetCode/0013.romanToInteger.cpp
@@ -27,52 +27,16 @@ public:
             else if (s[i] == 'M')
                 vec.push_back(1000);
         }
-        
         for (int i = 0; i < vec.size(); i++)
         {
-            if (vec[i]<vec[i+1])    // subtraction case
+            if (i + 1 < vec.size() && vec[i] < vec[i + 1]) // subtraction case
             {
-                
+                ans = ans + vec[i + 1] - vec[i];
+                i++;
             }
-            else    // normal case
-            {
-                
-            }
-            
+            else // normal case
+                ans += vec[i];
         }
-        
-        
-
-        // for (int i = 0; i < s.size(); i++)
-        // {
-        //     if (s[i] >= s[i + 1])
-        //     {
-        //         if (s[i] == 'I')
-        //             value = 1;
-        //         else if (s[i] == 'V')
-        //             value = 5;
-        //         else if (s[i] == 'X')
-        //             value = 10;
-        //         else if (s[i] == 'L')
-        //             value = 50;
-        //         else if (s[i] == 'C')
-        //             value = 100;
-        //         else if (s[i] == 'D')
-        //             value = 500;
-        //         else if (s[i] == 'M')
-        //             value = 1000;
-        //     }
-        //     else
-        //     {
-        //         if (s[i] == 'I' && s[i + 1] == 'V')
-        //             value = -1; // initially i setted value=4, but then for the next iteration V comes which adds 5, either skip or set value=-1
-        //         else if (s[i] == 'X' && s[i + 1] == 'L')
-        //             value = -10;
-        //         else if (s[i] == 'C' && s[i + 1] == 'D')
-        //             value = -100;
-        //     }
-        //     ans = ans + value;
-        // }
         return ans;
     }
 };
