1a980e9f914f1c5352031e2946ef800e1a5ba890
aura-uyt <aura.uyt@gmail.com>
2025-05-18 19:25:35 +0000

    DSA CWH Intro to Stack

---
+++ DSA/CWH-20/03.Stack/01.stackByArray.cpp
@@ -0,0 +1,25 @@
+/*
+Stack
+    Stack is a linear data structure
+    Operations in stack are performed in LIFO (last in first out) order
+    Insertion deletion happens from one end only
+
+        |     |
+        |  6  |
+        |  5  |  
+        |  3  |     this is like the stack of plates stored one above other
+        |_____|     last plate to come is the first one to go
+
+    Top ptr in stack represents the element which will pop first or simply which as at the top of stack
+    Pop()   => remove the topmost element from the stack
+    Push()  => adds an element into the stack
+    peek(index) => value at the given index from top is returned
+    isEmpty() / isFull()    => determines whether the stack is empty or full
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
