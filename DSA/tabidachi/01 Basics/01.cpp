5f4c776de0fc1360060fd7853d924699228a54fa
aura-uyt <aura.uyt@gmail.com>
2025-05-25 20:40:32 +0000

    DSA Day0 Flowchart typcast_ascii_int_to_char_viceversa

---
+++ DSA/tabidachi/01 Basics/01.cpp
@@ -0,0 +1,39 @@
+/*
+01: Intro to Programming & Flowcharts
+
+FLowchart
+    __________
+   (__________)     --> start/end (terminator)
+    __________
+   /_________/      --> Input/ Output
+    __________
+   |_________|      --> process
+
+      /\
+     /  \
+    /    \          --> Decision making
+    \   /
+     \/
+
+     ----->         --> arrows
+     <-----
+
+     ()             --> connector
+
+Pseudocode
+    generic way of representing code
+
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 'c';
+    cout << "ascii: " << a << endl;
+
+    char ch = 98;
+    cout << "char: " << ch << endl;
+
+    return 0;
+}
