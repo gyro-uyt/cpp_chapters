e14f9f7a80df3e124c9d2bd24f8fb51c792fddb6
aura-uyt <aura.uyt@gmail.com>
2025-05-26 18:05:27 +0000

    DSA Lec1-3 ✓

---
+++ DSA/tabidachi/01 Basics/02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Enter a: ";
+    int a = cin.get();  // it takes char as input
+    cout << "a: " << a << endl;
+
+    return 0;
+}
