04679af36175c35db4eef3c9ce99a10cafdf9884
aura-uyt <aura.uyt@gmail.com>
2025-06-02 02:54:21 +0530

    java D1

---
+++ java/BroCode/01/first/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.print("Yada Yada hi Dharmasaya\n");
+        System.out.println("WOW nice");
+        // these both lines are same
+        // java uses same C++ style comment syntax
+        // sout is shortcut for print line statemen
+        System.out.println("Just type sout");
+    }
+}
