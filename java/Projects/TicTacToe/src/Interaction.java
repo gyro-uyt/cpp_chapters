8065422382e79d037d6cb9f5ca00b334b5714054
aura-uyt <aura.uyt@gmail.com>
2025-06-13 23:43:09 +0530

    TicTacToe Base Completed

--- java/Projects/TicTacToe/src/Interaction.java
+++ java/Projects/TicTacToe/src/Interaction.java
@@ -7,6 +7,7 @@ public class Interaction {
     int userColumn;
     int computerRow;
     int computerColumn;
+    boolean gameOver = false;
     String playerChar;
     String computerChar;
     String[][] matrix = {
@@ -14,6 +15,7 @@ public class Interaction {
             {"-", "-", "-"},
             {"-", "-", "-"}
     };
+    static int countMoves;
 
     Scanner scanner = new Scanner(System.in);
     Random random = new Random();
@@ -40,20 +42,26 @@ public class Interaction {
     }
 
     void userMove() {
-        System.out.println("\nMake your move,");
-        System.out.print("Row (1/2/3): ");
-        userRow = scanner.nextInt();
-        this.userRow--;
 
-        System.out.print("Column (1/2/3): ");
-        userColumn = scanner.nextInt();
-        this.userColumn--;
+        boolean valid = false;
+        do {
+            System.out.println("\nMake your move,");
+            System.out.print("Row (1/2/3): ");
+            userRow = scanner.nextInt();
+            this.userRow--;
 
-        if (validMove(userRow, userColumn)) {
-            matrix[userRow][userColumn] = playerChar;
-        } else {
-            System.out.println("W: Make a Valid Move");
-        }
+            System.out.print("Column (1/2/3): ");
+            userColumn = scanner.nextInt();
+            this.userColumn--;
+
+            if (validMove(userRow, userColumn)) {
+                matrix[userRow][userColumn] = playerChar;
+                countMoves++;
+                valid = true;
+            } else {
+                System.out.println("W: Make a Valid Move");
+            }
+        } while (!valid);
         winCondition();
     }
 
@@ -64,6 +72,7 @@ public class Interaction {
             computerColumn = random.nextInt(3);
         } while (!validMove(computerRow, computerColumn));
         matrix[computerRow][computerColumn] = computerChar;
+        countMoves++;
         winCondition();
     }
 
@@ -74,41 +83,114 @@ public class Interaction {
     void winCondition() {
         // User Wins
         if (matrix[0][0].equals(matrix[0][1]) && matrix[0][0].equals(matrix[0][2]) && matrix[0][0].equals(playerChar)) {
+            System.out.println();
+            System.out.println("+-+-+-+-+");
             System.out.println("You, Wins");
+            System.out.println("+-+-+-+-+\n");
+            gameOver = true;
         } else if (matrix[1][0].equals(matrix[1][1]) && matrix[1][0].equals(matrix[1][2]) && matrix[1][0].equals(playerChar)) {
+            System.out.println();
+            System.out.println("+-+-+-+-+");
             System.out.println("You, Wins");
+            System.out.println("+-+-+-+-+\n");
+            gameOver = true;
         } else if (matrix[2][0].equals(matrix[2][1]) && matrix[2][0].equals(matrix[2][2]) && matrix[2][0].equals(playerChar)) {
+            System.out.println();
+            System.out.println("+-+-+-+-+");
             System.out.println("You, Wins");
+            System.out.println("+-+-+-+-+\n");
+            gameOver = true;
         } else if (matrix[0][0].equals(matrix[1][0]) && matrix[0][0].equals(matrix[2][0]) && matrix[0][0].equals(playerChar)) {
+            System.out.println();
+            System.out.println("+-+-+-+-+");
             System.out.println("You, Wins");
+            System.out.println("+-+-+-+-+\n");
+            gameOver = true;
         } else if (matrix[0][1].equals(matrix[1][1]) && matrix[0][1].equals(matrix[2][1]) && matrix[0][1].equals(playerChar)) {
+            System.out.println();
+            System.out.println("+-+-+-+-+");
             System.out.println("You, Wins");
+            System.out.println("+-+-+-+-+\n");
+            gameOver = true;
         } else if (matrix[0][2].equals(matrix[1][2]) && matrix[0][2].equals(matrix[2][2]) && matrix[0][2].equals(playerChar)) {
+            System.out.println();
+            System.out.println("+-+-+-+-+");
             System.out.println("You, Wins");
+            System.out.println("+-+-+-+-+\n");
+            gameOver = true;
         } else if (matrix[0][0].equals(matrix[1][1]) && matrix[0][0].equals(matrix[2][2]) && matrix[0][0].equals(playerChar)) {
+            System.out.println();
+            System.out.println("+-+-+-+-+");
             System.out.println("You, Wins");
+            System.out.println("+-+-+-+-+\n");
+            gameOver = true;
         } else if (matrix[0][2].equals(matrix[1][1]) && matrix[0][2].equals(matrix[2][0]) && matrix[0][2].equals(playerChar)) {
+            System.out.println();
+            System.out.println("+-+-+-+-+");
             System.out.println("You, Wins");
+            System.out.println("+-+-+-+-+\n");
+            gameOver = true;
         }
 
         // Computer Wins
         if (matrix[0][0].equals(matrix[0][1]) && matrix[0][0].equals(matrix[0][2]) && matrix[0][0].equals(computerChar)) {
+            System.out.println();
+            System.out.println("--------------");
             System.out.println("Computer, Wins");
+            System.out.println("--------------\n");
+            gameOver = true;
         } else if (matrix[1][0].equals(matrix[1][1]) && matrix[1][0].equals(matrix[1][2]) && matrix[1][0].equals(computerChar)) {
+            System.out.println();
+            System.out.println("--------------");
             System.out.println("Computer, Wins");
+            System.out.println("--------------\n");
+            gameOver = true;
         } else if (matrix[2][0].equals(matrix[2][1]) && matrix[2][0].equals(matrix[2][2]) && matrix[2][0].equals(computerChar)) {
+            System.out.println();
+            System.out.println("--------------");
             System.out.println("Computer, Wins");
+            System.out.println("--------------\n");
+            gameOver = true;
         } else if (matrix[0][0].equals(matrix[1][0]) && matrix[0][0].equals(matrix[2][0]) && matrix[0][0].equals(computerChar)) {
+            System.out.println();
+            System.out.println("--------------");
             System.out.println("Computer, Wins");
+            System.out.println("--------------\n");
+            gameOver = true;
         } else if (matrix[0][1].equals(matrix[1][1]) && matrix[0][1].equals(matrix[2][1]) && matrix[0][1].equals(computerChar)) {
+            System.out.println();
+            System.out.println("--------------");
             System.out.println("Computer, Wins");
+            System.out.println("--------------\n");
+            gameOver = true;
         } else if (matrix[0][2].equals(matrix[1][2]) && matrix[0][2].equals(matrix[2][2]) && matrix[0][2].equals(computerChar)) {
+            System.out.println();
+            System.out.println("--------------");
             System.out.println("Computer, Wins");
+            System.out.println("--------------\n");
+            gameOver = true;
         } else if (matrix[0][0].equals(matrix[1][1]) && matrix[0][0].equals(matrix[2][2]) && matrix[0][0].equals(computerChar)) {
+            System.out.println();
+            System.out.println("--------------");
             System.out.println("Computer, Wins");
+            System.out.println("--------------\n");
+            gameOver = true;
         } else if (matrix[0][2].equals(matrix[1][1]) && matrix[0][2].equals(matrix[2][0]) && matrix[0][2].equals(computerChar)) {
+            System.out.println();
+            System.out.println("--------------");
             System.out.println("Computer, Wins");
+            System.out.println("--------------\n");
+            gameOver = true;
         }
 
+        // tie
+        if (countMoves==9 && !gameOver){
+            System.out.println();
+            System.out.println("***********");
+            System.out.println("It's a Draw");
+            System.out.println("***********\n");
+            gameOver=true;
+        }
     }
+
 }
--- java/Projects/TicTacToe/src/Main.java
+++ java/Projects/TicTacToe/src/Main.java
@@ -2,28 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         Interaction interaction = new Interaction();
-        Core core = new Core();
         Display display = new Display();
 
         interaction.welcome();
-
-        display.showMatrix(interaction.matrix);
-        interaction.userMove();
         display.showMatrix(interaction.matrix);
 
-        interaction.computerMove();
-        display.showMatrix(interaction.matrix);
+        while (!interaction.gameOver) {
+            interaction.userMove();
+            display.showMatrix(interaction.matrix);
 
-        interaction.userMove();
-        display.showMatrix(interaction.matrix);
+            if (interaction.gameOver) {
+                break;
+            }
 
-        interaction.computerMove();
-        display.showMatrix(interaction.matrix);
-
-        interaction.userMove();
-        display.showMatrix(interaction.matrix);
-
-        interaction.computerMove();
-        display.showMatrix(interaction.matrix);
+            interaction.computerMove();
+            display.showMatrix(interaction.matrix);
+        }
     }
 }
