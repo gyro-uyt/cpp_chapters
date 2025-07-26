08d392898d3229937ee060fc70c69b7402c51dac
aura-uyt <aura.uyt@gmail.com>
2025-07-26 22:57:01 +0530

    Webdev D17.2

---
+++ WebDev/CWH/JS/03/index.js
@@ -0,0 +1,10 @@
+// we'll run via node
+console.log("This can be run through coderunner too!");
+console.log("Did you know we can use terminal as tab too!");
+
+let age = 45;
+if (age > 18) {
+  console.log("You can drive");
+} else {
+  console.log("You cannot drive");
+}
