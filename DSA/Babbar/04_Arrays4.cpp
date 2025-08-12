01dd6a80eff3a51529a1dd6e666b9aac964ba450
Tokita <aura.uyt@gmail.com>
2025-08-13 01:23:33 +0530

    DSA D4.1

--- DSA/Babbar/04_Arrays4.cpp
+++ DSA/Babbar/04_Arrays4.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
 int linearSearch(int arr[], int size, int target) {
   for (int i = 0; i < size; i++) {
     if (arr[i] == target) {
@@ -10,10 +17,27 @@ int linearSearch(int arr[], int size, int target) {
   return -1;
 }
 
-int main() {
-  int array[] = {34, 73, 9, 7, 15, 84};
+void reverseArray(int arr[], int size) {
+  int left = 0;
+  int right = size - 1;
+  while (left <= right) {
+    int temp = arr[right];
+    arr[right] = arr[left];
+    arr[left] = temp;
+    left++;
+    right--;
+  }
+}
 
-  cout << linearSearch(array, 6, 84) << endl;
+int main() {
+  int array[] = {1, 3, 5, 7, 9};
+  int size = sizeof(array) / sizeof(array[0]);
+  cout << "Size: " << size << endl;
+  cout << linearSearch(array, size, 84) << endl;
+
+  printArray(array, size);
+  reverseArray(array, size);
+  printArray(array, size);
 
   return 0;
 }
