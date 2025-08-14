c6ee9dd177bd8ddc6900aac3441e78c32ac6b5c3
Tokita <aura.uyt@gmail.com>
2025-08-14 23:57:17 +0530

    DSA D5

--- DSA/Babbar/05.cpp
+++ DSA/Babbar/05.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
-#include "../utils/utils.h"
 using namespace std;
 
-int main(){
-    int arr[] = {23,234,23,42,645,34,5};
+int binarySearch(int arr[], int size, int target) {
+  int left = 0;
+  int right = size - 1;
 
-    printArray(arr, 7);
+  while (left <= right) {
+    int middle = left + (right - left) / 2;
+    if (arr[middle] == target) {
+      return middle;
+    } else if (arr[middle] <= target) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return -1;
+}
 
-    return 0;
+int main() {
+  int arr[] = {23, 234, 23, 42, 645, 34, 5};
+
+  cout << binarySearch(arr, 7, 645) << endl;
+
+  return 0;
 }
