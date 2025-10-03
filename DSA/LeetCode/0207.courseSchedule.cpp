6276165afcc6bd4f7c398450d8d4344dac52460d
gyro <aura.uyt@gmail.com>
2025-10-03 23:13:32 +0530

    Learn Kahn's algo to solve LC207

---
+++ DSA/LeetCode/0207.courseSchedule.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+This problem introduces the concept of directed graphs, cycle detection, and can
+be solved via DFS (with visited + recursion stack) or Kahn’s algorithm
+(topological sort with BFS / indegree).
+*/
+class Solution {
+public:
+  bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
+
+    return false;
+  }
+};
