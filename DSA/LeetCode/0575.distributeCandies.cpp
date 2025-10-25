b26f8ff0c1c415557f696f04334c667d96da9ec4
gyro <aura.uyt@gmail.com>
2025-10-26 00:07:30 +0530

    0575 Distribute Candies (set)

---
+++ DSA/LeetCode/0575.distributeCandies.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int distributeCandies(vector<int> &candyType) {
+    set<int> store;
+
+    for (auto i : candyType)
+      store.insert(i);
+
+    int n = candyType.size();
+    int diffTypesCandies = store.size();
+
+    return min(n / 2, diffTypesCandies);
+  }
+};
