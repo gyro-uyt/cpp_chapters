34a190f6de0fa349512fc38bee1a0275a75ecb4d
gyro <aura.uyt@gmail.com>
2025-11-02 23:18:55 +0530

    1929 Concatenation of Array

---
+++ DSA/LeetCode/1929.concatenationArray.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2*n, -1);
+
+        for (int i = 0; i < n; i++)
+              ans[i]=ans[n+i]=nums[i];
+
+        return ans;
+    }
+};
