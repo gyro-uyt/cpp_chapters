bf42ce51b2de1e820eb4dc69d09dbd1de8a0ff2b
gyro <aura.uyt@gmail.com>
2025-11-04 01:22:34 +0530

    JS D14.1

---
+++ WebDev/JS/CaC/36.DOM.js
@@ -0,0 +1,14 @@
+// Document Object Model (DOM)
+
+// for html/browser page, hence following command will not work here, only on browser
+// console.log(Window.document);
+// console.log(document); // same as above
+// document == Window.document
+
+// HTMLCollection, NodeCollection, Array are all different things
+
+// to select an element by ID present in HTML
+// document.getElementById("first-heading");
+
+// Now after selection we can apply operations on it
+// document.getElementById("first-heading").innerHTML = "<h1>Saitama</h1>";
