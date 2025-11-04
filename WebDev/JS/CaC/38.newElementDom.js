088bc104114e80515b012277b716100c839ba83f
gyro <aura.uyt@gmail.com>
2025-11-05 03:51:46 +0530

    Removing /C++/SFML/SFML-2.6.0

---
+++ WebDev/JS/CaC/38.newElementDom.js
@@ -0,0 +1,8 @@
+const parent = document.querySelector(".parent");
+console.log(parent);
+console.log(parent.children);
+console.log(parent.children[2]);
+console.log(parent.children[2].innerHTML);
+
+
+
