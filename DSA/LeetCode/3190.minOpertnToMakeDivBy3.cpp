d7d1201c71a7d737d9e77f1f203bf6facd58489d
gyro <aura.uyt@gmail.com>
2025-11-23 00:06:09 +0530

    3190 MinOpertnToMakeDivBy3

---
+++ DSA/LeetCode/3190.minOpertnToMakeDivBy3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int minimumOperations(vector<int> &nums) {
+    int count = 0;
+    for (int num : nums)
+      if (num % 3 != 0)
+        count++;
+
+    return count;
+  }
+};
