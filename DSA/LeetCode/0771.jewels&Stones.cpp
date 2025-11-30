d6b396c1a2e8ee923ca0f943fd4c198fb620a37b
gyro <aura.uyt@gmail.com>
2025-12-01 01:04:16 +0530

    0771 Jewel & Stone

---
+++ DSA/LeetCode/0771.jewels&Stones.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int numJewelsInStones(string jewels, string stones) {
+    int count = 0;
+    for (char stone : stones)
+      for (char jewel : jewels)
+        if (stone == jewel)
+          count++;
+    return count;
+  }
+};
