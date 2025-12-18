7a3ed656b8a27eade436bcc5b6c8398a9ce80e01
gyro <aura.uyt@gmail.com>
2025-12-18 23:57:21 +0530

    Starting C

---
+++ C/rough.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  // printf("hello, world\n");
+  // int x = 4;
+  char apple[] = "hello";
+  printf("%s\n", apple);
+  printf("%u\n", apple[1]);
+  printf("%c\n", apple+3);
+  printf("%u\n", apple+4);
+  printf("%u\n", apple+5);
+  printf("%u\n", apple+6);
+  return 0;
+}
