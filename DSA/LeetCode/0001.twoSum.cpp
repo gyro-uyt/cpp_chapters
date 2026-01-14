14d25077c6da773034240cb85433dfd523be2c8f
gyro <aura.uyt@gmail.com>
2026-01-14 23:01:48 +0530

    DSA D1

--- DSA/LeetCode/0001.twoSum.cpp
+++ DSA/LeetCode/0001.twoSum.cpp
@@ -1,43 +1,48 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
-        for (int i = 0; i < nums.size(); i++)
-        {
-            for (int j = 0; j < nums.size(); j++)
-            {
-                if (i != j && nums[i] + nums[j] == target)
-                {
-                    return {i, j};
-                }
-            }
+  vector<int> twoSum(vector<int> &nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+      for (int j = 0; j < nums.size(); j++) {
+        if (i != j && nums[i] + nums[j] == target) {
+          return {i, j};
         }
-        return {};
+      }
     }
+    return {};
+  }
+  class Solution1 {
+  public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+      unordered_map<int, int> numToIndex;
+      for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (numToIndex.find(complement) != numToIndex.end()) {
+          return {numToIndex[complement], i};
+        }
+        numToIndex[nums[i]] = i;
+      }
+      return {};
+    }
+  };
 };
 
-int main()
-{
-    int target = 6;
-    vector<int> vec = {1, 4, 4, 2, 44, 53};
-    vector<int> soln;
+int main() {
+  int target = 6;
+  vector<int> vec = {1, 4, 4, 2, 44, 53};
+  vector<int> soln;
 
-    Solution s1;
-    soln = s1.twoSum(vec, target);
-    for (int i = 0; i < soln.size(); i++)
-    {
-        cout << soln[i];
-        if (i != (soln.size() - 1))
-        {
-            cout << ", ";
-        }
+  Solution s1;
+  soln = s1.twoSum(vec, target);
+  for (int i = 0; i < soln.size(); i++) {
+    cout << soln[i];
+    if (i != (soln.size() - 1)) {
+      cout << ", ";
     }
-    cout << endl;
+  }
+  cout << endl;
 
-    return 0;
+  return 0;
 }
---
+++ Resources/Obsidian/Reflections.md
@@ -0,0 +1,6 @@
+What am I doing as of now ?
+-  Only easy leetcode questions at night
+-  Reading CSAPP book during evening
+-  Learning to use Neovim
+-  Documenting what I use for fun
+-  Exploring new techs like wayland, termux, etc.
