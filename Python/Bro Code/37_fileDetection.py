d5c8a8398de6398bb006f0d70f777a80f8256911
aura-uyt <aura.uyt@gmail.com>
2026-04-16 22:53:31 +0530

    Pyday 12

--- Python/Bro Code/37_fileDetection.py
+++ Python/Bro Code/37_fileDetection.py
@@ -1,2 +1,16 @@
 import os
 
+# Note: '\' may causes errors in string while writing address due to it being escape sequence character, hence use '|' or '\\'
+file_path = "C:/Users/HP/Desktop/test"
+
+if os.path.exists(file_path):
+    print(f"The location '{file_path}' exists")
+
+if os.path.isfile(file_path):
+    print("That is a file")
+elif os.path.isdir(file_path):
+    print("That is a directory")
+
+else:
+    print("That location doesn't exist")
+ 
