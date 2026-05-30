87dfa655a7e3c1cc38856f58394c0d233d09ff4e
gyro-uyt <aura.uyt@gmail.com>
2026-05-30 19:12:51 +0000

    0121 buy/sell stock

--- DSA/LeetCode/0121.bestTimeToBuySellStock.cpp
+++ DSA/LeetCode/0121.bestTimeToBuySellStock.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <climits>
 #include <iostream>
 #include <vector>
@@ -14,6 +15,27 @@ public:
       } else if ((prices[i] - minPrice) > maxProfit) {
         maxProfit = prices[i] - minPrice;
       }
+    }
+    return maxProfit;
+  }
+  int bruteForce(vector<int> &prices) {
+    int maxProfit = 0;
+    int curr = 0;
+    int n = prices.size();
+    for (int i = 0; i < n; i++)
+      for (int j = i + 1; j < n; j++) {
+        curr = prices[j] - prices[i];
+        maxProfit = max(maxProfit, curr);
+      }
+    return maxProfit;
+  }
+  int optimal(vector<int> &prices) {
+    int maxProfit = 0;
+    int mini = prices[0];
+    for (int i = 1; i < prices.size(); i++) {
+      int profit = prices[i] - mini;
+      maxProfit = max(maxProfit, profit);
+      mini = min(mini, prices[i]);
     }
     return maxProfit;
   }
