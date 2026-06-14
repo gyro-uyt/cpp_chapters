af9dcdb06d93865f5f9cc14b5df34e562f17cb16
gyro-uyt <aura.uyt@gmail.com>
2026-06-14 22:45:00 +0000

    0056 Merge interval optimal fixed

--- DSA/LeetCode/0056.mergeIntervals.cpp
+++ DSA/LeetCode/0056.mergeIntervals.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 class Solution {
 public:
-  vector<vector<int>> optimal(vector<vector<int>> &intervals) {
+  vector<vector<int>> merge(vector<vector<int>> &intervals) {
     sort(intervals.begin(), intervals.end());
     vector<vector<int>> ans;
     for (int i = 0; i < intervals.size(); i++) {
-      if (ans.empty() || intervals[i][0] >= ans.back()[1])
+      if (i == 0 || ans.back()[1] < intervals[i][0])
         ans.push_back(intervals[i]);
       else
         ans.back()[1] = max(ans.back()[1], intervals[i][1]);
