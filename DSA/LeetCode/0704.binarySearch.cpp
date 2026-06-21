fd6b9dbfa689bff188c22c0b48d10b6d028c7250
gyro-uyt <aura.uyt@gmail.com>
2026-06-21 21:17:31 +0000

    BS

--- DSA/LeetCode/0704.binarySearch.cpp
+++ DSA/LeetCode/0704.binarySearch.cpp
@@ -3,19 +3,39 @@ using namespace std;
 
 class Solution {
 public:
-  int search(vector<int> &nums, int target) {
-    int left = 0;
-    int right = nums.size() - 1;
-    while (left <= right) {
-      int middle = left + (right - left) / 2;
-      if (nums[middle] == target) {
-        return middle;
-      } else if (nums[middle] >= target) {
-        right = middle - 1;
-      } else {
-        left = middle + 1;
-      }
+  // iterative Solution
+  int search1(vector<int> &nums, int target) {
+    int low = 0;
+    int high = nums.size() - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] == target) {
+        while (nums[mid] == nums[mid - 1]) // returns the first occurance
+          mid--;
+        return mid;
+      } else if (nums[mid] > target)
+        high = mid - 1;
+      else
+        low = mid + 1;
     }
     return -1;
+  }
+
+  // recursive Solution
+  int bs(vector<int> &nums, int target, int low, int high) {
+    if (low > high)
+      return -1;
+    int mid = low + (high - low) / 2;
+    if (nums[mid] == target) {
+      while (nums[mid] == nums[mid - 1])
+        mid--;
+      return mid;
+    } else if (target < nums[mid])
+      return bs(nums, target, low, mid - 1);
+    else
+      return bs(nums, target, mid + 1, high);
+  }
+  int search2(vector<int> &nums, int target) {
+    return bs(nums, target, 0, nums.size() - 1);
   }
 };
